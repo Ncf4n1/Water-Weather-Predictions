497cacefda38c2114ed3e4136b5374302b53b7c6
Logan Vining <logan.l.vining@gmail.com>
2021-12-15 01:39:32 -0700

    use datetime obj, str time not implemented yet????

--- consumer/influx_consumer.py
+++ consumer/influx_consumer.py
@@ -2,8 +2,8 @@ import json
 import os
 import s2cell
 from datetime import datetime
-from influxdb_client import InfluxDBClient
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import ASYNCHRONOUS
 from kafka import KafkaConsumer
 
 INFLUX_ORG = os.environ.get("INFLUX_ORG")
@@ -19,7 +19,7 @@ def consume(topic: str):
         enable_auto_commit=True,
         group_id="water-weather-consumers",
         value_deserializer=lambda msg: json.loads(msg.decode()),
-        consumer_timeout_ms=2000
+        consumer_timeout_ms=500
     )
 
     messages = []
@@ -28,25 +28,33 @@ def consume(topic: str):
 
         code = message.value.get("code")
         name = message.value.get("name")
-        discharge = message.value.get("discharge")
+        discharge = float(message.value.get("discharge"))
 
         time_obj = datetime.strptime(message.value.get("time"), '%Y-%m-%dT%H:%M:%S.%f%z')
-        recorded_at = int(time_obj.timestamp() * 1000000000)
 
         lat = message.value.get("geolocation").get("latitude")
         lon = message.value.get("geolocation").get("longitude")
 
         cell_id = s2cell.lat_lon_to_token(lat, lon)
 
-        messages.append(f"test_point,location={code},s2_cell_id={cell_id} station_name=\"{name}\",discharge={discharge},lat={lat},lon={lon}")
+        messages.append(
+            Point("gauge_marker") \
+            .tag("location", code) \
+            .tag("s2_cell_id", cell_id) \
+            .field("station_name", name) \
+            .field("discharge", discharge) \
+            .field("lat", lat) \
+            .field("lon", lon) \
+            .time(time_obj)
+        )
 
     _insert_messages(messages)
 
-def _insert_messages(messages: list[str]):
+def _insert_messages(messages):
     """Write messages to the Influx Cloud hydrograph_time_series bucket"""
     with InfluxDBClient(org=INFLUX_ORG, token=INFLUX_TOKEN, url="https://europe-west1-1.gcp.cloud2.influxdata.com") as client:
 
-        writer = client.write_api(write_options=SYNCHRONOUS)
+        writer = client.write_api(write_options=ASYNCHRONOUS)
 
         writer.write(INFLUX_BUCKET, INFLUX_ORG, messages)
         print(f"WROTE messages to bucket")
